c98715b25162c1b7898bb255b2d88919874dd9a7
merchancano123 <miguel.merchancano@eia.edu.co>
2025-07-24 17:06:10 -0500

    se arreglo el envio constante de mensajes tipos twist en el control remoto

--- custom_pkg/module_to_import.py
+++ custom_pkg/module_to_import.py
@@ -20,6 +20,7 @@ class JoystickToTwistNode(Node):
 
         self.subscription = self.create_subscription(Joy, 'joy', self.joy_callback, 10)
         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.bandera = 0
 
 
     def joy_callback(self, msg):
@@ -27,24 +28,27 @@ class JoystickToTwistNode(Node):
 
         # Validación de botón habilitador
         if self.require_enable_button:
-            if len(msg.buttons) <= self.enable_button or msg.buttons[self.enable_button] != 1:
-                twist.linear.x = 0.0
-                twist.angular.z = 0.0
-                self.publisher.publish(twist) 
-                return
+                if len(msg.buttons) <= self.enable_button or msg.buttons[self.enable_button] != 1:
+                        if self.bandera == 1:
+                            twist.linear.x = 0.0
+                            twist.angular.z = 0.0
+                            self.bandera = 0
+                            self.publisher.publish(twist) 
+                            return
 
-        if len(msg.axes) > max(self.linear_axis, self.angular_axis):
-            linear_input = msg.axes[1]
-            angular_input = msg.axes[0]
+                elif len(msg.axes) > max(self.linear_axis, self.angular_axis):
+                    self.bandera = 1
+                    linear_input = msg.axes[1]
+                    angular_input = msg.axes[0]
 
-            # Aplicar zona muerta
-            linear_input = 0.0 if abs(linear_input) < self.deadzone else linear_input
-            angular_input = 0.0 if abs(angular_input) < self.deadzone else angular_input
+                    # Aplicar zona muerta
+                    linear_input = 0.0 if abs(linear_input) < self.deadzone else linear_input
+                    angular_input = 0.0 if abs(angular_input) < self.deadzone else angular_input
 
-            twist.linear.x = self.linear_scale * linear_input
-            twist.angular.z = self.angular_scale * angular_input
+                    twist.linear.x = self.linear_scale * linear_input
+                    twist.angular.z = self.angular_scale * angular_input
 
-            self.publisher.publish(twist)
-            self.get_logger().debug(f"cmd_vel -> linear.x={twist.linear.x:.2f}, angular.z={twist.angular.z:.2f}")
-        else:
-            self.get_logger().warn("Mensaje Joy no contiene suficientes ejes.")
+                    self.publisher.publish(twist)
+                    self.get_logger().debug(f"cmd_vel -> linear.x={twist.linear.x:.2f}, angular.z={twist.angular.z:.2f}")
+                else:
+                    self.get_logger().warn("Mensaje Joy no contiene suficientes ejes.")
